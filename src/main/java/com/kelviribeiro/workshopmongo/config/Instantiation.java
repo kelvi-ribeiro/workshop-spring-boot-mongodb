b00e3b1d23e077592e792a84448f430f14da193e
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-08-07 00:05:16 -0300

    Aula 177. Operacai de instanciacao da base de dados

---
+++ src/main/java/com/kelviribeiro/workshopmongo/config/Instantiation.java
@@ -0,0 +1,27 @@
+package com.kelviribeiro.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.kelviribeiro.workshopmongo.domain.User;
+import com.kelviribeiro.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.deleteAll();
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));	
+	}
+
+}
